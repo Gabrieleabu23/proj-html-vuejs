4aaf18f80b9772c329a0919289d181eb6b148749
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-01-17 18:07:52 +0100

    add: listanime object in store

---
+++ src/store.js
@@ -0,0 +1,104 @@
+import { reactive } from "vue";
+export const store= reactive({
+    animeList:[
+        {
+            id:1,
+            path:'../assets/anime/assets/rice-ball.webp',
+            category:'Food',
+            title:'Hygenic receipe to prepare rice',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:2,
+            path:'../assets/anime/meal-time-150x150.webp',
+            category:'Food',
+            title:'The best time to have a meal',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:3,
+            path:'../assets/anime/assets/healthy-foods.webp',
+            category:'Food',
+            title:'The best healthy foods',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:4,
+            path:'../assets/anime/assets/winter.webp',
+            category:'Fashion',
+            title:'The best winter outfits',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:5,
+            path:'../assets/anime/assets/photographers-mistakes.webp',
+            category:'Fashion',
+            title:'Beginner photographer’s mistakes',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:6,
+            path:'../assets/anime/ideas-anime.webp',
+            category:['Culture','Stories'],
+            title:'Live Ideas you might be anime',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:7,
+            path:'../assets/anime/assets/visit-france.webp',
+            category:['Lifestyle','Travel'],
+            title:'Reasons to visit france',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:8,
+            path:'../assets/anime/assets/travel-alone.webp',
+            category:['Travel','Stories'],
+            title:'Traveling alone is awesome',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:9,
+            path:'../assets/anime/assets/success-story.webp',
+            category:['Culture','Stories'],
+            title:'The best success stories',
+            date:'December 26, 2022',
+            user:'demo'
+        },
+        {   
+            id:10,
+            path:'../assets/anime/assets/best-places.webp',
+            category:['Lifestyle','Stories','Travel'],
+            title:'Places for a road trip',
+            date:'December 25, 2022',
+            user:'demo'
+        },
+        {   
+            id:11,
+            path:'../assets/anime/music-love.webp',
+            category:['Culture','Lifestyle'],
+            title:'Music the Love of my life',
+            date:'December 25, 2022',
+            user:'demo'
+        },
+        {   
+            id:12,
+            path:'../assets/anime/assets/anime-fashion.webp',
+            category:['Fashion','Lifestyle'],
+            title:'Fashion trend now a days',
+            date:'December 25, 2022',
+            user:'demo'
+        },
+
+    ]
+
+    
+})
